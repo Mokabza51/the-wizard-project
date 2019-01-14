fc362f515aab625fdb47ee7c3e5d357b6aea74b2
Mohamed Kaba <44191113+Mokabza51@users.noreply.github.com>
2019-01-14 17:12:38 +0100

    Update OutfitBk.java

--- src/main/java/fr/wildcodeschool/thewizardproject/models/OutfitBk.java
+++ src/main/java/fr/wildcodeschool/thewizardproject/models/OutfitBk.java
@@ -1,7 +1,11 @@
 package fr.wildcodeschool.thewizardproject.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class OutfitBk implements OutfitInterface {
 
+	@Override
 	public String changeDress() {
 		return "The wizard's dress is black";
 	}
